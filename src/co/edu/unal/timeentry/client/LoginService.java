18e3559f83605b419f685810759001d938deff49
Sergio Gabalan <seresgavillar@hotmail.com>
2015-06-19 22:19:39 +0000

    add TimeEntry application developed during the course

--- src/co/edu/unal/timeentry/client/LoginService.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unal.timeentry.client;
-
-import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-import com.google.gwt.user.client.rpc.RemoteService;
-
-@RemoteServiceRelativePath("login")
-public interface LoginService extends RemoteService {
-	public LoginInfo login(String requesUri);
-}
--- src/co/edu/unal/timeentry/client/LoginServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package co.edu.unal.timeentry.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface LoginServiceAsync {
-	public void login(String requestUri, AsyncCallback<LoginInfo> asyinc);
-}
